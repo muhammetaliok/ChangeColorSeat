06c34ba6dd1731aaeb9cf91dd3674a4bf44b3fbc
Muhammet Ali OK <61350755+muhammetaliok@users.noreply.github.com>
2022-08-09 17:17:17 +0300

    Update change.js

--- ChangeColor/static/scripts/change.js
+++ ChangeColor/static/scripts/change.js
@@ -1,5 +1,6 @@
 let color =document.querySelector(".color");
 let car = document.querySelector(".car");
+let head_car= document.querySelector(".head_car")
 
 let block = document.querySelectorAll(".block");
 
@@ -7,10 +8,30 @@ let block = document.querySelectorAll(".block");
     block[i].addEventListener('click', ()=> {
       let colorBlock = window.getComputedStyle(block[i]).backgroundColor;
       car.style.backgroundColor = colorBlock;
+      head_car.backgroundColor = colorBlock;
     })
     
- }
+}
+
 
 color.addEventListener("input", ()=> {
   car.style.background = color.value;
 })
+const colorx = document.querySelector("button")
+  colorx.addEventListener("click", ()=> {
+    head_car.style.background = color.value;
+})
+    
+
+
+
+
+
+// const colorx = document.querySelector("button")
+// colorx.addEventListener("click", ()=>{
+//   color.addEventListener("input", ()=>{
+//     head_car.style.background = color.value;
+//   })
+  
+//})
+
