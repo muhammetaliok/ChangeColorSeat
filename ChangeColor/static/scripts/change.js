ccc474eb922fed81eae0fc2aac8b6f7996530456
Muhammet Ali OK <61350755+muhammetaliok@users.noreply.github.com>
2022-08-17 16:21:55 +0300

    Update change.js
    Change multiple image color with option and put image to another image background

--- ChangeColor/static/scripts/change.js
+++ ChangeColor/static/scripts/change.js
@@ -1,37 +1,65 @@
-let color =document.querySelector(".color");
-let car = document.querySelector(".car");
-let head_car= document.querySelector(".head_car")
+function hexToRgb(color) {
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  color = color.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+  });
+  
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+  return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+  } : {
+      r: 0,
+      g: 0,
+      b: 0
+  };
+}
 
-let block = document.querySelectorAll(".block");
+function colorImage(imgId,hexaColor) {
+  // create hidden canvas (using image dimensions)
+  var imgElement = document.getElementById(imgId);
+  
+  var canvas = document.createElement("canvas");
+  canvas.width = imgElement.width;
+  canvas.height = imgElement.height;
 
-  for(let i=0; i<block.length; i++) {
-    block[i].addEventListener('click', ()=> {
-      let colorBlock = window.getComputedStyle(block[i]).backgroundColor;
-      car.style.backgroundColor = colorBlock;
-      head_car.style.backgroundColor = colorBlock;
-    })
-    
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(imgElement,0,0);
+  
+  var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+  
+  var data = imageData.data;
+
+  // convert image to grayscale
+  var rgbColor = hexToRgb(hexaColor);
+  
+  for(var p = 0, len = data.length; p < len; p+=4) {
+      //if(data[p+3] == 0)
+      //   continue;
+      data[p + 0] = rgbColor.r;
+      data[p + 1] = rgbColor.g;
+      data[p + 2] = rgbColor.b;
+      //data[p + 3] = 255;
+  }
+  ctx.putImageData(imageData, 0, 0);
+
+  // replace image source with canvas data
+  imgElement.src = canvas.toDataURL();
+}
+
+// changing color of capsule on select event
+document.getElementById('sel_top').onchange = function(){
+  colorImage('img_top', this.value);
+}
+document.getElementById('sel_mid').onchange = function(){
+  colorImage('img_mid', this.value);
+}
+document.getElementById('ilkResim').onclick = function(){
+  document.getElementById('img_bottom').style.backgroundImage = "url('https://www.gezginfoto.com.tr/images/makaleler/etkili_manzara_11.jpg')"
 }
 
 
-color.addEventListener("input", ()=> {
-  car.style.background = color.value;
-})
-const colorx = document.querySelector("button")
-  colorx.addEventListener("click", ()=> {
-    head_car.style.background = color.value;
-})
-    
-
-
-
-
-
-// const colorx = document.querySelector("button")
-// colorx.addEventListener("click", ()=>{
-//   color.addEventListener("input", ()=>{
-//     head_car.style.background = color.value;
-//   })
   
 //})
 
